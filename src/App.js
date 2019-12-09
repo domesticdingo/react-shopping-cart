81ecbd84514855de56e1e3dc9d7e10468feced7f
Indigo Richards <thedomesticdingo@gmail.com>
2019-12-09 10:42:08 -0800

    MVP complete

--- src/App.js
+++ src/App.js
@@ -7,35 +7,47 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		const newItem = {
+			id: item.id,
+			title: item.title,
+			price: item.price,
+			image: item.image
+		}
+		setCart({...cart, newItem})
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value={{ cart }}>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route
+					exact
+					path="/"
+					render={() => (
+						<Products
+							products={products}
+							addItem={addItem}
+						/>
+					)}
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+				<Route exact path="/" component={Products} />
+			</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
